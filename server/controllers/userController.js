86c4822ad12fa6e22879e676ee96612cd556628f
theEdith <the.edith.2802@gmail.com>
2023-12-12 22:23:17 +0530

    Updated the DB URL

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,8 +2,6 @@ import asyncHandler from "express-async-handler";
 
 import { prisma } from "../config/prismaConfig.js";
 
-
-
 export const createUser = asyncHandler(async (req, res) => {
   console.log("creating a user");
 
